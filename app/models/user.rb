5f49a13f376090803a9756aef800b0afd8db319a
ayman <aymanshalaby539@gmail.com>
2026-06-08 10:31:46 +0300

    added users using scaffoldi

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,2 @@
 class User < ApplicationRecord
-  validates :name, presence: true
-  validates :dob, presence: true
-  validates :phone_number, presence: true
-  validates :email,
-            presence: true,
-            uniqueness: true,
-            format: { with: URI::MailTo::EMAIL_REGEXP, message: "must be a valid email address" }
 end
