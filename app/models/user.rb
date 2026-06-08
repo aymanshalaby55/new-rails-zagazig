bebc69587dedb658e30e90604eb44b6fd8ae978c
ayman <aymanshalaby539@gmail.com>
2026-06-08 10:39:05 +0300

    implement mvc lab

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
+  has_many :created_posts,
+    class_name: "Post",
+    foreign_key: "user_id"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,43 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+User.destroy_all
+Post.destroy_all
+Editor.destroy_all
+PostEditor.destroy_all
+
+user1 = User.create!(
+  name: "Ayman",
+  email: "ayman@example.com"
+)
+
+user2 = User.create!(
+  name: "Ali",
+  email: "ali@example.com"
+)
+
+post1 = Post.create!(
+  title: "Rails MVC",
+  body: "Learning Rails associations",
+  creator: user1
+)
+
+post2 = Post.create!(
+  title: "Active Record",
+  body: "Understanding relationships",
+  creator: user2
+)
+
+editor1 = Editor.create!(
+  name: "Sara",
+  email: "sara@example.com"
+)
+
+editor2 = Editor.create!(
+  name: "Omar",
+  email: "omar@example.com"
+)
+
+PostEditor.create!(post: post1, editor: editor1)
+PostEditor.create!(post: post1, editor: editor2)
+PostEditor.create!(post: post2, editor: editor1)
